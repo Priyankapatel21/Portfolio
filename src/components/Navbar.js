29c2125a7749ac71cb99f078aa652180884eacd1
Priyanka Patel <ppriyankapatel58@gmail.com>
2026-03-26 16:44:22 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -112,7 +112,7 @@ function NavBar() {
             </Nav.Item>
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Priyankapatel21/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
